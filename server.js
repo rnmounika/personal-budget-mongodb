e58d59d2ec6e342c3df2fdcaf0fee2a37ecd6c94
Ramapuram Naik Mounika <r.n.mounika@gmail.com>
2020-10-02 00:33:22 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -3,22 +3,22 @@ const app = express();
 const port =3000;
 app.use('/',express.static('public'));
 
-const budget =
-{myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 100
-    }
+ const budget =require('./body.json');
+// {myBudget: [
+//     {
+//         title: 'Eat out',
+//         budget: 25
+//     },
+//     {
+//         title: 'Rent',
+//         budget: 375
+//     },
+//     {
+//         title: 'Grocery',
+//         budget: 100
+//     }
 
-]};
+// ]};
 
 app.get('/hello',(req, res) =>{
     res.send('Hello World 1');
